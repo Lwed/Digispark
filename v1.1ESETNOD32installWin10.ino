ef3ff0f9ae704515457b6acf7293c79090ec1109
Leo <53151152+Lwed@users.noreply.github.com>
2021-09-23 09:47:44 -0700

    Update v1.1ESETNOD32installWin10.ino

--- v1.1ESETNOD32installWin10.ino
+++ v1.1ESETNOD32installWin10.ino
@@ -9,22 +9,22 @@ void setup() {
 
 void loop() {
   DigiKeyboard.sendKeyStroke(0); // wakes keyboard if necessary
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(3500);
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT); // opens menu
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(3500);
   DigiKeyboard.sendKeyStroke(KEY_A); // selects powershell as admin
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(3000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT); // yes
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(3500);
   DigiKeyboard.println("Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))");
   DigiKeyboard.delay(15000);
   DigiKeyboard.println("exit");
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT); // opens menu
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(3500);
   DigiKeyboard.sendKeyStroke(KEY_A); // selects powershell as admin
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(3000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT); // yes
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(3500);
   DigiKeyboard.println("choco install eset-nod32-antivirus --upgrade --ignore-checksums --force -a -y; sleep 3"); //installs ESET NOD32 AntiVirus
   DigiKeyboard.delay(50000);
   DigiKeyboard.println("Set-ExecutionPolicy Restricted"); 
